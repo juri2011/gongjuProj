74e85d765fc2ba9058e752eb8230fe48f6e15823
오주리 <ojl98@naver.com>
2024-01-23 15:00:24 +0900

    MainController에서 입력 명령어 실행 로직 추가

--- src/ProjectManagement/MainController.java
+++ src/ProjectManagement/MainController.java
@@ -23,8 +23,44 @@ public class MainController {
     while(true) {
       help();
       String userInput = sc.nextLine().trim();
+      String purpose = "";
       //명령어: 1. 추가
       if(userInput.equals("1")) {
+        
+        //사용자로부터 관리할 프로젝트의 용도 입력
+        System.out.println("새로 추가할 프로젝트의 용도를 입력하세요. (개인/비지니스)");
+        userInput = sc.nextLine().trim();
+        if(userInput.equals("개인")) purpose = "personal";
+        else if(userInput.equals("비지니스")) purpose = "business";
+        else {
+          System.out.println("잘 못 입력하셨습니다. \'개인\'과 \'비지니스\' 중 하나를 입력하세요.");
+        }
+        
+        //사용자로부터 프로젝트의 내용 입력
+        System.out.println("추가할 프로젝트를 입력하세요.");
+        System.out.println("(형식)제목 중요도 시작일(yyMMdd) 종료일(yyMMdd) 상태(진행/보류/완료)");
+        //사용자에게 입력형식에 대한 설명 출력
+        System.out.println("-------설명------");
+        System.out.println("제목 : 프로젝트의 제목입니다. 중복될 수 없습니다.");
+        System.out.println("중요도 : 미정 / 하 / 중 / 상");
+        System.out.println("시작일 : 프로젝트가 시작되는 날짜입니다.");
+        System.out.println("종료일 : 프로젝트가 끝나는 날짜입니다.");
+        System.out.println("상태 : 프로젝트의 제목입니다.");
+        System.out.println("---------------");
+        
+        userInput = sc.nextLine();
+        String[] userInputs = userInput.split(" ");
+        
+        //데이터 유효성 검사
+        if(userInputs.length != 5) {
+          System.out.println("입력 형식이 올바르지 않습니다.");
+          continue;
+        }
+        
+        //RequestDTO에서 올바른 데이터 형식으로 작성했는지 검사 필요
+        //RequestDTO dto = new RequestDTO(purpose, userInputs[0], userInputs[1], userInputs[2], userInputs[3], userInputs[4]);
+        
+        //ProjectAddServices addSrv = new ProjectAddServices();
         
       }
       //명령어: 2. 삭제
